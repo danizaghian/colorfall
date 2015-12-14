6ac4f040d50c8a1b18210ed40eb95443e79bb15d
danielle <dani.zaghian@gmail.com>
2015-12-13 21:10:11 -0800

    twitter test16

--- server.js
+++ server.js
@@ -77,13 +77,13 @@ app.get('/gameresult/:id', function (req, res, next){
        else if(socialScraper =='twitter') {
         // temp data to make it work (in validator)
         // https://dev.twitter.com/cards/types/summary-large-image
-        desc1 ="I made Art!" <br> + "- The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here.";
+        desc1 ="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here.";
         html +="<meta name='twitter:card' content='summary_large_image' />"+
         "<meta name='twitter:site' content='@ColorfallGame' />"+
         "<meta name='twitter:creator' content='@ColorfallGame' />"+
         "<meta name='twitter:url' content='"+meta.url+"' />"+
         "<meta name='twitter:title' content='"+meta.title+"' />"+
-        "<meta name='twitter:description' content='"+desc1+"' />"+
+        "<meta property='og:description' content='In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling.'"+
         "<meta name='twitter:image:src' content='"+meta.image+"' />";
       }
       html +="</head>"+
