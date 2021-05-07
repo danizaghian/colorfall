5817728f5e08546296d53d348486358d18460bcd
dani <dani.zaghian@gmail.com>
2021-05-07 16:36:17 -0700

    node modules update

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost/colorfall");
+mongoose.createConnection( process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost/colorfall");
 
 module.exports.GameResult = require("./gameresult.js");
 
