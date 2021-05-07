5a91f4fc221a0ff62557f0adb6ab1c9426fa39ba
dani <dani.zaghian@gmail.com>
2021-05-07 16:44:46 -0700

    updates index.js to use correct mongo syntax

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.createConnection( process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost/colorfall");
+mongoose.createConnection( process.env.MONGODB_URI || process.env.MONGOHQ_URL || "mongodb://localhost:27017/colorfall");
 
 module.exports.GameResult = require("./gameresult.js");
 
