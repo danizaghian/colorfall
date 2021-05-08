e0f45000f83d3cf7019ca392c998a586f0204171
dani <dani.zaghian@gmail.com>
2021-05-07 17:12:38 -0700

    fix: refactor hard coded connection string to Heroku config var

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.createConnection( process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://127.0.0.1/colorfall");
+mongoose.createConnection( process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.MONGODB_URI || "mongodb://127.0.0.1/colorfall");
 
 module.exports.GameResult = require("./gameresult.js");
 
